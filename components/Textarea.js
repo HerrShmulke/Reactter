cd422022cddc481500a34444af7aa91cd34e2d3b
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2021-02-23 16:35:15 +0700

    feat(textarea) add counter and custom footer
    Custom footer is needed to insert custom components into textarea.

--- components/Textarea.js
+++ components/Textarea.js
@@ -8,9 +8,12 @@ import styles from '../styles/Textarea.module.css';
  * @param {string} param0.placeholder
  * @param {number} param0.maxLength
  * @param {string} param0.name
+ * @param {boolean} param0.counter
+ * @param {JSX.Element} param0.CustomFooter
+ * @param {import('react').DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>} param0.props
  * @param {import('react').Dispatch<import('react').SetStateAction<string>>} param0.setValue
  */
-export default function Textarea({ value, setValue, placeholder, maxLength, name }) {
+export default function Textarea({ value, setValue, placeholder, maxLength, counter, CustomFooter, ...props }) {
   let textareaRef = useRef(null);
 
   const updateValue = (element) => {
@@ -29,14 +32,33 @@ export default function Textarea({ value, setValue, placeholder, maxLength, name
   });
 
   return (
-    <textarea
-      value={value}
-      onChange={updateValue}
-      placeholder={placeholder}
-      className={styles.textarea}
-      ref={textareaRef}
-      onKeyDown={preventEnter}
-      name={name}
-    ></textarea>
+    <div>
+      <textarea
+        {...props}
+        value={value}
+        className={styles.textarea}
+        onChange={updateValue}
+        onKeyDown={preventEnter}
+        ref={textareaRef}
+      ></textarea>
+
+      <div className={styles.footer}>
+        {counter ? (
+          <span className={`${styles.counter} ${value.length >= maxLength - maxLength * 0.1 ? styles.redCounter : ''}`}>
+            {value.length}/{maxLength}
+          </span>
+        ) : (
+          ''
+        )}
+
+        {CustomFooter ? (
+          <div className={styles.customFooter}>
+            <CustomFooter />
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
   );
 }
