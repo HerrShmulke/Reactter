ebe290b47f11ad8cce7e66037eb6eca23f6d7352
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2021-02-26 21:33:53 +0700

    feat(modal) Add closing modal window to Escape button

--- components/Modal.js
+++ components/Modal.js
@@ -3,16 +3,28 @@ import { useEffect, useRef } from 'react';
 import styles from '../styles/Modal.module.css';
 
 export default function Modal({ children, ActionsComponent, setActive }) {
+  /**
+   * @type {import('react').MutableRefObject<HTMLDivElement>}
+   */
+  let modalRef = useRef(null);
+
   useEffect(() => {
     document.body.style.overflow = 'hidden';
+    modalRef.current.focus();
 
     return () => {
       document.body.style.overflow = 'unset';
     };
-  });
+  }, [modalRef]);
 
   return (
-    <div className={styles.overlay}>
+    <div
+      className={styles.overlay}
+      onKeyUp={(event) => event.code.toLowerCase() === 'escape' && setActive(false)}
+      tabIndex='-1'
+      style={{ outline: 'none' }}
+      ref={modalRef}
+    >
       <div className={styles.card}>
         <div className={styles.closeModal}>
           <Image
