9cc0889bb54f1d2e9c6185ad40945bac7b28dc07
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2021-02-27 02:15:40 +0700

    feat(modal) add focus lock

--- components/Modal.js
+++ components/Modal.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { useEffect, useRef } from 'react';
 import styles from '../styles/Modal.module.css';
+import FocusLock from 'react-focus-lock';
 
 export default function Modal({ children, ActionsComponent, setActive }) {
   /**
@@ -18,26 +19,31 @@ export default function Modal({ children, ActionsComponent, setActive }) {
   }, [modalRef]);
 
   return (
-    <div
-      className={styles.overlay}
-      onKeyUp={(event) => event.code.toLowerCase() === 'escape' && setActive(false)}
-      tabIndex='-1'
-      style={{ outline: 'none' }}
-      ref={modalRef}
-    >
-      <div className={styles.card}>
-        <div className={styles.closeModal}>
-          <Image
-            src='/close-modal.svg'
-            width={28}
-            height={28}
-            alt='Закрыть модальное окно'
-            onClick={() => setActive(false)}
-          />
+    <FocusLock>
+      <div
+        className={styles.overlay}
+        onKeyUp={(event) => event.code.toLowerCase() === 'escape' && setActive(false)}
+        tabIndex='-1'
+        style={{ outline: 'none' }}
+        ref={modalRef}
+      >
+        <div className={styles.card}>
+          <div className={styles.closeModal}>
+            <Image
+              src='/close-modal.svg'
+              width={28}
+              height={28}
+              alt='Закрыть модальное окно'
+              onClick={() => setActive(false)}
+              onKeyUp={(event) => event.code.toLowerCase() === 'space' && setActive(false)}
+              role='button'
+              tabIndex='0'
+            />
+          </div>
+          <div>{children}</div>
+          <div className={styles.actions}>{ActionsComponent ? <ActionsComponent /> : ''}</div>
         </div>
-        <div>{children}</div>
-        <div className={styles.actions}>{ActionsComponent ? <ActionsComponent /> : ''}</div>
       </div>
-    </div>
+    </FocusLock>
   );
 }
