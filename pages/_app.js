1246605f142658142af0c508075bdce03c1c7820
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2021-03-13 05:34:22 +0700

    feat(index) add subscripe to add post

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,38 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { API_URI } from '../lib/constants';
+import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from 'apollo-link-ws';
+import { API_URI, WS_URI } from '../lib/constants';
 import '../styles/globals.css';
 
-const client = new ApolloClient({
+const wsLink = process.browser
+  ? new WebSocketLink({
+      uri: WS_URI,
+      options: {
+        reconnect: true,
+      },
+    })
+  : null;
+
+const httpLink = new HttpLink({
   uri: API_URI,
+});
+
+const link = process.browser
+  ? split(
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+        return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+      },
+      wsLink,
+      httpLink
+    )
+  : null;
+
+const client = new ApolloClient({
+  link: link,
   cache: new InMemoryCache(),
   credentials: 'same-origin',
 });
-
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useSubscription } from '@apollo/client';
 import Head from 'next/head';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '../components/Button';
 import Container from '../components/Container';
 import Header from '../components/Header';
@@ -9,13 +9,28 @@ import Modal from '../components/Modal';
 import PostCard from '../components/PostCard';
 import SubmitReactt from '../components/SubmitReactt';
 import Textarea from '../components/Textarea';
-import { getAllPosts } from '../lib/post';
 import { getUser } from '../lib/user';
 import styles from '../styles/Index.module.css';
 
 const GET_POSTS = gql`
   query GetPosts($take: Int!, $skip: Int!) {
     posts(take: $take, skip: $skip) {
+      id
+      message
+      owner {
+        name
+        avatarUrl
+      }
+      likesCount
+      commentsCount
+      isLikes
+    }
+  }
+`;
+
+const POSTS_SUBSCRIPTION = gql`
+  subscription {
+    postAdded {
       id
       message
       owner {
@@ -74,10 +89,26 @@ function AddCommentActions(setValue) {
 }
 
 export default function Home({ user }) {
-  const { loading, data: posts } = useQuery(GET_POSTS, { variables: { skip: 0, take: 2 } });
+  const { loading: postsLoading, data: posts } = useQuery(GET_POSTS, { variables: { skip: 0, take: 10 } });
   const [activeModal, setActiveModal] = useState(false);
   const [commentMessage, setCommentMessage] = useState('');
   const [currentPost, setCurrentPost] = useState(null);
+  const [postPool, setPostPool] = useState([]);
+  const { data: subPost, loading: subPostLoading } = useSubscription(POSTS_SUBSCRIPTION);
+
+  useEffect(() => {
+    if (!postsLoading) {
+      setPostPool((oldArr) => [...posts.posts]);
+      console.log('new render');
+    }
+  }, [posts]);
+
+  useEffect(() => {
+    if (!subPostLoading) {
+      setPostPool((oldArr) => [subPost.postAdded, ...oldArr]);
+      console.log('old render');
+    }
+  }, [subPost]);
 
   return (
     <div className={styles.container}>
@@ -102,22 +133,21 @@ export default function Home({ user }) {
         <Header user={user} />
         <SubmitReactt />
         <div className={styles.posts}>
-          {!loading &&
-            posts.posts.map((post) => (
-              <PostCard
-                key={post.id}
-                avatarUrl={post.owner.avatarUrl}
-                authorName={post.owner.name}
-                commentsCount={post.commentsCount}
-                likesCount={post.likesCount}
-                message={post.message}
-                isLikes={post.isLikes}
-                onCommentClick={() => {
-                  setActiveModal(true);
-                  setCurrentPost(post);
-                }}
-              />
-            ))}
+          {postPool.map((post) => (
+            <PostCard
+              key={post.id}
+              avatarUrl={post.owner.avatarUrl}
+              authorName={post.owner.name}
+              commentsCount={post.commentsCount}
+              likesCount={post.likesCount}
+              message={post.message}
+              isLikes={post.isLikes}
+              onCommentClick={() => {
+                setActiveModal(true);
+                setCurrentPost(post);
+              }}
+            />
+          ))}
         </div>
       </Container>
     </div>
